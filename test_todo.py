33ce5418eb1d40551aa848742f268352b834e320
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-02-09 20:30:58 +0300

    Add some API test with example

--- test_todo.py
+++ test_todo.py
@@ -1,7 +1,7 @@
 import os
 import pytest
-from playwright.sync_api import Playwright
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import Page, Route, expect
 
 
 def test_add_todo(playwright: Playwright) -> None:
@@ -378,3 +378,95 @@ def test_todo(page):
     input_field.press('Enter')
     todo_item = page.get_by_test_id('todo-item')
     expect(todo_item).to_have_count(2)
+
+
+#Мониторинг сетевых запросов
+def test_listen_network(page: Page):
+    page.on("request", lambda request: print(">>", request.method, request.url))
+    page.on("response", lambda response: print("<<", response.status, response.url))
+    page.goto('https://osinit.ru/')
+
+
+#Модифицировать запрос
+def test_network(page):
+    page.route("**/register", lambda route: route.continue_(post_data='{"email": "user","password": "secret"}'))
+    page.goto('https://reqres.in/')
+    page.get_by_text(' Register - successful ').click()
+
+
+#Модифицировать ответ
+def test_mock_tags(page):
+    page.route("**/api/tags", lambda route: route.fulfill(path="data.json"))
+    page.goto('https://demo.realworld.io/#/')
+
+
+#Мокинг данных
+def test_intercepted(page: Page):
+    def handle_route(route: Route):
+        response = route.fetch()
+        json = response.json()
+        json["tags"] = ["open", "solutions"]
+        route.fulfill(json=json)
+
+    page.route("**/api/tags", handle_route)
+
+    page.goto("https://demo.realworld.io/#/")
+    sidebar = page.locator('css=div.sidebar')
+    expect(sidebar.get_by_role('link')).to_contain_text(["open", "solutions"])
+
+
+#Воспроизведение из HAR???
+def test_replace_from_har(page):
+    page.goto("https://reqres.in/")
+    page.route_from_har("example.har")
+    users_single = page.locator('li[data-id="users-single"]')
+    users_single.click()
+    response = page.locator('[data-key="output-response"]')
+    expect(response).to_contain_text("Open Solutions")
+
+
+#???
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(service_workers="block")
+    context.route_from_har("C:\\Users\\Valentin\\PycharmProjects\\python_playwright\\example.har", url="**/api/users/2")
+    page = context.new_page()
+    page.locator("html").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
+#Тестирование API - GET
+def test_inventory(page):
+    response = page.request.get('https://petstore.swagger.io/v2/store/inventory')
+    print(f'\nСтатус код: {response.status}')
+    print(f'Тело ответа: {response.json()}')
+
+
+#Тестирование API - POST
+def test_add_user(page):
+    data = [
+              {
+                "id": 9743,
+                "username": "fsd",
+                "firstName": "fff",
+                "lastName": "ggg",
+                "email": "bbb",
+                "password": "tt",
+                "phone": "333",
+                "userStatus": 0
+              }
+            ]
+    header = {
+        'accept': 'application/json',
+        'content-Type': 'application/json'
+    }
+    response = page.request.post('https://petstore.swagger.io/v2/user/createWithArray', data=data, headers=header)
+    print(f'\nСтатус код: {response.status}')
+    print(f'Тело ответа: {response.json()}')
